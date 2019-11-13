d8c770025145c678ebb2e8c5693fe94974641013
Rachel Woodlock <rachelmwoodlock@gmail.com>
2019-11-12 22:32:51 -0500

    Revert "Linked to resource details page"
    This reverts commit 564e276715c2241dfb76350f6336613fc0dca606.

--- HHPhillyApp/HHPhillyApp/EntryPage.xaml.cs
+++ HHPhillyApp/HHPhillyApp/EntryPage.xaml.cs
@@ -14,6 +14,11 @@ namespace HHPhillyApp
             searchResults.ItemsSource = DataService.ResourceNames;
         }
 
+        //protected override async void OnAppearing()
+        //{
+            //base.OnAppearing();
+           // searchResults.ItemsSource = await App.Database.GetItemsAsync();
+        //} 
         void OnTextChanged(object sender, EventArgs e)
         {
             SearchBar searchBar = (SearchBar)sender;
@@ -24,11 +29,6 @@ namespace HHPhillyApp
         {
             SearchBar bar = (SearchBar)sender;
             searchResults.ItemsSource = DataService.GetSearchResults(bar.Text);
-        }
-
-        void OnClicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new ResourceDetails());
         }
 
     }
