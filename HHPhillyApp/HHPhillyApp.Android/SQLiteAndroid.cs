059e8031a8a3b54e9d6fb1aabaf22beb6631c20e
kbarta8 <30446303+kbarta8@users.noreply.github.com>
2019-11-24 16:27:24 -0500

    changes to android db file

--- HHPhillyApp/HHPhillyApp.Android/SQLiteAndroid.cs
+++ HHPhillyApp/HHPhillyApp.Android/SQLiteAndroid.cs
@@ -17,32 +17,14 @@ namespace HHPhillyApp.Droid
         #region ISQLite implementation
         public SQLite.Net.SQLiteConnection GetConnection()
         {
-            var sqliteFilename = "HHPhillyDB";
-            string documentsPath = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            var sqliteFilename = "HHPHillyDB";
+            string documentsPath = Environment.GetFolderPath(Environment.SpecialFolder.Personal); // Documents folder
             var path = Path.Combine(documentsPath, sqliteFilename);
-
-            // This is where we copy in the prepopulated database
             Console.WriteLine(path);
-
-            if (!File.Exists(path))
-            {
-                using (var binaryReader = new BinaryReader(Android.App.Application.Context.Assets.Open(sqliteFilename)))
-                {
-                    using (var binaryWriter = new BinaryWriter(new FileStream(path, FileMode.Create)))
-                    {
-                        byte[] buffer = new byte[2048];
-                        int length = 0;
-                        while ((length = binaryReader.Read(buffer, 0, buffer.Length)) > 0)
-                        {
-                            binaryWriter.Write(buffer, 0, length);
-                        }
-                    }
-                }
-            }
-
+            if (!File.Exists(path)) File.Create(path);
             var plat = new SQLite.Net.Platform.XamarinAndroid.SQLitePlatformAndroid();
             var conn = new SQLite.Net.SQLiteConnection(plat, path);
-
+            // Return the database connection 
             return conn;
         }
         #endregion
