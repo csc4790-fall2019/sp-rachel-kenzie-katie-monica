2ecd5dd2a2acda093a08ed0891850c2e848e54da
Rachel Woodlock <rachelmwoodlock@gmail.com>
2019-11-25 19:31:11 -0500

    working drop down

--- HHPhillyApp/HHPhillyApp/EntryPage.xaml.cs
+++ HHPhillyApp/HHPhillyApp/EntryPage.xaml.cs
@@ -23,10 +23,30 @@ namespace HHPhillyApp
         void OnTextChanged(object sender, EventArgs e)
         {
             SearchBar searchBar = (SearchBar)sender;
+            lstData.ItemsSource = App.Database.GetAllMen();
            // lstData.ItemsSource = DataService.GetSearchResults(searchBar.Text);
             //searchResults.ItemsSource = getSearchResults(searchBar.Text);
            // searchResults.ItemsSource = DataService.GetSearchResults(searchBar.Text);
             //searchResults.ItemsSource = DataService.GetSearchResults(searchBar.Text);
+        }
+
+        void OnPickerSelectedIndexChanged(object sender, EventArgs e)
+        {
+            var picker = (Picker)sender;
+            int selectedIndex = picker.SelectedIndex;
+
+            if (selectedIndex == 0)
+            {
+                lstData.ItemsSource = App.Database.GetAllResources();
+            }
+            if (selectedIndex == 1)
+            {
+                lstData.ItemsSource = App.Database.GetAllMen();
+            }
+            if (selectedIndex == 2)
+            {
+                lstData.ItemsSource = App.Database.GetAllWomen();
+            }
         }
 
         void OnSearchButtonPressed(object sender, EventArgs e)
